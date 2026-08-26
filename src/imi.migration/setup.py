f77a9ddef2d62e713a9c791f4c02dcfb9d6462d2
Gregor Traven <gregor.traven@gmail.com>
2026-08-26 15:46:59 +0200

    Add imi.migration package metadata

---
+++ src/imi.migration/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='imi.migration',
+    version='0.1.0',
+    description='Dexterity content models and migration tooling for the IMI Plone 4.3 to 5.2 migration',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['imi'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Plone',
+        'plone.api',
+        'plone.app.dexterity',
+        'collective.easyform==3.2.1',
+    ],
+)
