03ce5dcc3fc0596acfca248c908b509064c4b49b
Gregor Traven <gregor.traven@gmail.com>
2026-08-27 10:19:43 +0200

    Initialize theming registry before repairing Barceloneta profiles

--- tools/plone52_repair_theme_profiles.py
+++ tools/plone52_repair_theme_profiles.py
@@ -2,12 +2,30 @@
 # -*- coding: utf-8 -*-
 """Install missing Plone 5 theming profiles on existing target sites."""
 
+from plone.app.theming.interfaces import IThemeSettings
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+from zope.component.hooks import setSite
+
 SITES = ('portal', 'dezurstva', 'kiestra', 'preiskave', 'nadomescanja')
 PROFILES = (
     'profile-plone.app.theming:default',
     'profile-plonetheme.barceloneta:default',
     'profile-imi.migration:default',
 )
+
+
+def ensure_theme_registry(site):
+    setSite(site)
+    registry = getUtility(IRegistry)
+    try:
+        settings = registry.forInterface(IThemeSettings)
+    except KeyError:
+        registry.registerInterface(IThemeSettings)
+        settings = registry.forInterface(IThemeSettings)
+    if settings.currentTheme is None:
+        settings.currentTheme = u''
+    return settings
 
 
 def run(app):
@@ -19,12 +37,19 @@ def run(app):
             print('missing: /%s' % site_id)
             continue
         site = app[site_id]
+        setSite(site)
         setup = site.portal_setup
-        for profile in PROFILES:
-            setup.runAllImportStepsFromProfile(profile)
-            print('/%s: applied %s' % (site_id, profile))
-        updated += 1
-    transaction.commit()
+        try:
+            ensure_theme_registry(site)
+            for profile in PROFILES:
+                setup.runAllImportStepsFromProfile(profile)
+                print('/%s: applied %s' % (site_id, profile))
+            transaction.commit()
+            updated += 1
+        except Exception as exc:
+            transaction.abort()
+            print('/%s: ERROR %r' % (site_id, exc))
+    setSite(None)
     print('Sites repaired: %d' % updated)
 
 
