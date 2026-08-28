3a99a69e15553701040b648c37b49a714c58383e
Gregor Traven <gregor.traven@gmail.com>
2026-08-28 18:49:48 +0200

    Use direct physical Preiskave search and mode-aware views

--- src/imi.migration/src/imi/migration/browser/exams_runtime.py
+++ src/imi.migration/src/imi/migration/browser/exams_runtime.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 """Public Preiskave views independent of catalog/workflow state."""
-from .legacy_sites import ExamsPublicView as BaseExamsPublicView
-from .legacy_sites import ExamsListView as BaseExamsListView
-from .runtime_fixes import ExamsMixin
+from .public_fixes import ExamsPublicView, ExamsListView, ExamsLiveSearchView
 
-
-class ExamsPublicView(ExamsMixin, BaseExamsPublicView):
-    pass
-
-
-class ExamsListView(ExamsMixin, BaseExamsListView):
-    pass
+__all__ = ('ExamsPublicView', 'ExamsListView', 'ExamsLiveSearchView')
