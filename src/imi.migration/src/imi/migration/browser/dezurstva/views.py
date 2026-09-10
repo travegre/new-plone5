8b463f00af9c7cf2b374367dbcfc625a1c0cec18
Gregor Traven <gregor.traven@gmail.com>
2026-09-10 18:50:36 +0200

    Use site-local Dežurstva implementation imports

--- src/imi.migration/src/imi/migration/browser/dezurstva/views.py
+++ src/imi.migration/src/imi/migration/browser/dezurstva/views.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
-"""Template and static-resource ownership for Dežurstva."""
+"""Dežurstva browser views."""
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from ..legacy.duty_compat import DutyChangeRequestView as BaseDutyChangeRequestView
-from ..legacy.public_fixes import DutyPublicView as BaseDutyPublicView
-from ..legacy.staff_import import StaffImportView as BaseStaffImportView
+
+from .duty_compat import DutyChangeRequestView as BaseDutyChangeRequestView
+from .public_fixes import DutyPublicView as BaseDutyPublicView
+from .staff_import import StaffImportView as BaseStaffImportView
+
 
 class DutyPublicView(BaseDutyPublicView):
     template = ViewPageTemplateFile('duty_public.pt')
+
+
 class StaffImportView(BaseStaffImportView):
     template = ViewPageTemplateFile('staff_import.pt')
+
+
 class DutyChangeRequestView(BaseDutyChangeRequestView):
     def __call__(self):
         rendered = super(DutyChangeRequestView, self).__call__()
         if not isinstance(rendered, str):
             return rendered
-        return rendered.replace('/++resource++imi.migration/duty-public.css','/++resource++imi.migration/dezurstva/duty-public.css')
+        return rendered.replace(
+            '/++resource++imi.migration/duty-public.css',
+            '/++resource++imi.migration/dezurstva/duty-public.css')
