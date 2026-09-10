08f5da8d658de2d0556df6ef6f3fd4142946f257
Gregor Traven <gregor.traven@gmail.com>
2026-09-10 18:56:26 +0200

    Remove Imenik dependency on mixed legacy views

--- src/imi.migration/src/imi/migration/browser/imenik/views.py
+++ src/imi.migration/src/imi/migration/browser/imenik/views.py
@@ -1,13 +1,44 @@
 # -*- coding: utf-8 -*-
-"""Template ownership for the IMI imenik frontend."""
+"""IMI imenik browser views."""
+from plone import api
+from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from ..legacy.imports import DirectoryImportView as BaseDirectoryImportView
-from ..legacy.legacy_admin import DirectoryAdminView as BaseDirectoryAdminView
-from ..legacy.legacy_sites import DirectoryPublicView as BaseDirectoryPublicView
 
-class DirectoryPublicView(BaseDirectoryPublicView):
+from .imports import DirectoryImportView
+
+
+class DirectoryPublicView(BrowserView):
     template = ViewPageTemplateFile('directory_public.pt')
-class DirectoryAdminView(BaseDirectoryAdminView):
+
+    def __call__(self):
+        return self.template()
+
+    @property
+    def portal(self):
+        return api.portal.get()
+
+    def categories(self):
+        base = self.portal.get('data2')
+        if base is None:
+            return []
+        result = []
+        for folder in base.objectValues():
+            if getattr(folder, 'portal_type', None) != 'Folder':
+                continue
+            children = [child for child in folder.objectValues()
+                        if getattr(child, 'portal_type', None) == 'Folder']
+            result.append({
+                'title': folder.Title() or folder.getId(),
+                'children': [child.Title() or child.getId() for child in children],
+            })
+        return result
+
+    def is_editor(self):
+        return api.user.has_permission('Modify portal content', obj=self.portal)
+
+
+class DirectoryAdminView(DirectoryPublicView):
     template = ViewPageTemplateFile('directory_admin.pt')
-class DirectoryImportView(BaseDirectoryImportView):
-    template = ViewPageTemplateFile('directory_import.pt')
+
+    def __call__(self):
+        return self.template()
